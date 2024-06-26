15a0c67198b16a621246261897d45acaaf11ab8e
Sergei Nerobeev <sergeinerobeev@gmail.com>
2024-06-26 23:07:42 +0200

    not correct data Subway test

--- src/test/java/SubwayTest.java
+++ src/test/java/SubwayTest.java
@@ -27,6 +27,18 @@ class SubwayTest {
         Assertions.assertFalse(actual);
     }
 
+    @DisplayName("isLineWithThisColorExists - not correct data - Line color doesn't exist!")
+    @Test
+    void isColorExistsInlines_notCorrect() {
+        String cityName = "Budapest";
+        String colorLine = "Red";
+        Subway subway = new Subway(cityName);
+
+        boolean actual = subway.isLineWithThisColorExists(colorLine);
+
+        Assertions.assertTrue(actual);
+    }
+
     @DisplayName("isStationNameExistsInAnyLine - correct data - station already exists!")
     @Test
     void isStationNameInlines() {
@@ -36,6 +48,17 @@ class SubwayTest {
         boolean actual = subway.isStationNameExistsInAnyLine(cityName);
 
         Assertions.assertFalse(actual);
+    }
+
+    @DisplayName("isStationNameExistsInAnyLine - not correct data - station already exists!")
+    @Test
+    void isStationNameInlines_notCorrect() {
+        String cityName = "Budapest";
+        Subway subway = new Subway(cityName);
+
+        boolean actual = subway.isStationNameExistsInAnyLine(cityName);
+
+        Assertions.assertTrue(actual);
     }
 
     @DisplayName("checkLineIsEmpty - correct data - LineNotEmptyException")
@@ -49,10 +72,20 @@ class SubwayTest {
         });
     }
 
+    @DisplayName("checkLineIsEmpty - not correct data - LineNotEmptyException")
+    @Test
+    void checkLineIsEmptyTest_notCorrect() throws NoSuchMethodException {
+        Line line = null;
+        Method checkLineIsEmptyMethod = Subway.class.getDeclaredMethod("checkLineIsEmpty", Line.class);
+        checkLineIsEmptyMethod.setAccessible(true);
+        Assertions.assertThrows(LineNotEmptyException.class, () -> {
+            checkLineIsEmptyMethod.invoke(Subway.class,line);
+        });
+    }
     @DisplayName("createNewLine - correct data - new line created")
     @Test
     void createNewLineTest() {
-        String colorLine = "Red";
+        var colorLine = "Red";
         var cityName = "Budapest";
         Subway subway = new Subway(cityName);
 
@@ -60,6 +93,20 @@ class SubwayTest {
 
         Assertions.assertNotNull(line);
         Assertions.assertEquals(colorLine, line.getColor());
+    }
+
+    @DisplayName("createNewLine - not correct data - new line not created")
+    @Test
+    void createNewLineTest_notCorrect() {
+        var colorLine = "Red";
+        var wrongColorLine = "Blue";
+        var cityName = "Budapest";
+        Subway subway = new Subway(cityName);
+
+        Line line = subway.createNewLine(colorLine);
+
+        Assertions.assertNotNull(line);
+        Assertions.assertEquals(wrongColorLine, line.getColor());
     }
 
     @DisplayName("createFirstStation - correct data - first station created")
@@ -76,7 +123,23 @@ class SubwayTest {
         Station exptStation = subway.createFirstStation(colorLine, nameStation, transferStations);
 
         Assertions.assertEquals(exptStation, actlStation);
+    }
 
+    @DisplayName("createFirstStation - not correct data - first station not created")
+    @Test
+    void createFirstStationTest_notCorrect() {
+        var colorLine = "White";
+        var wrongColorLine = "Blue";
+        var nameStation = "Октогон";
+        var cityName = "Budapest";
+        Subway subway = new Subway(cityName);
+        Line line = subway.createNewLine(colorLine);
+        List<Station> transferStations = List.of();
+
+        Station actlStation = new Station(nameStation, line, transferStations, subway);
+        Station exptStation = subway.createFirstStation(wrongColorLine, nameStation, transferStations);
+
+        Assertions.assertEquals(exptStation, actlStation);
     }
 
 }
